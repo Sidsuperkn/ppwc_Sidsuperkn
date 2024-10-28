429e3457b884e4d7691eccff3425431dedfdc230
SIDDHARTH KUMAR <122125106+Sidsuperkn@users.noreply.github.com>
2024-10-28 23:53:44 +0530

    Update q3.c

--- 28-oct-2024/q3.c
+++ 28-oct-2024/q3.c
@@ -4,15 +4,15 @@ int main() {
     int zero_count = 0;
     int minus_sum = 0;
     int plus_sum = 0;
-    int x;
-    printf("Enter a value for x: ");
-    scanf("%d", &x);
-    if (x == 0) {
+    int y;
+    printf("Enter a value for y: ");
+    scanf("%d", &y);
+    if (y == 0) {
         zero_count += 1;
-    } else if (x < 0) {
-        minus_sum += x;
+    } else if (y < 0) {
+        minus_sum += y;
     } else {
-        plus_sum += x;
+        plus_sum += y;
     }
     printf("Zero count: %d\n", zero_count);
     printf("Minus sum: %d\n", minus_sum);
