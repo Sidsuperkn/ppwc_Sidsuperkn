6f20b5d2f3ceb8444e8487bd981cba5ef004944a
SIDDHARTH KUMAR <122125106+Sidsuperkn@users.noreply.github.com>
2024-10-29 11:08:29 -0700

    Update q1.c

--- 29-oct-2024/q1.c
+++ 29-oct-2024/q1.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int number, modifiedNumber = 0;
+    int place = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    while (number > 0) {
+        int digit=number%10;
+
+        
+        if (digit==0) {
+            modifiedNumber+=1*place;
+        } else if (digit==1) {
+            modifiedNumber += 0*place;
+        } else {
+            modifiedNumber += digit*place; 
+        }
+
+        number/=10;
+        place*=10;  
+    }
+
+    printf("Modified number: %d\n", modifiedNumber);
+
+    return 0;
+}
+
 
